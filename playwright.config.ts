cefbb2302c8afa2252a69a84dd6dbb74d52bea6b
Jake Tolentino <jgtolentino.rn@gmail.com>
2025-10-07 04:12:16 +0800

    fix: always reuse existing server in Playwright tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
   fullyParallel: true,
   reporter: 'list',
   use: { timezoneId: 'Asia/Manila', locale: 'en-PH', colorScheme: 'dark' },
-  webServer: { command: 'pnpm start', url: 'http://localhost:3001', reuseExistingServer: !process.env.CI },
+  webServer: { command: 'pnpm start', url: 'http://localhost:3001', reuseExistingServer: true },
   projects: [ { name: 'chromium', use: { ...devices['Desktop Chrome'], viewport: { width: 1280, height: 900 } } } ]
 });
